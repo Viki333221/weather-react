d4bcc3a30658e4dae0ec8d1ce7d6df17a2e4dced
Viki333221 <vika333221@gmail.com>
2022-11-17 15:16:07 -0500

    Linked Github repository URL

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Weather from "./Weather.js"
 
 export default function App() {
   return (
     <div className="App">
-          Hi 😃
+      <div className="container">
+<Weather />
+<p>
+  This project was coded by {" "}<a href="#" target="_blank" > Viktoriia Suvorov </a>{" "} and is {" "} <a href="https://github.com/Viki333221/weather-react" target="_blank">{" "} open-souced on Github </a>
+</p>
+      </div>
     </div>
   );
 }
